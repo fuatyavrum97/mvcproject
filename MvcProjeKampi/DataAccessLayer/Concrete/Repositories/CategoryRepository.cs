01695a63dec1b520fff9001a1b8ea9412966f32b
fuatyavrum97 <fuatcan.yavrum@ceng.deu.edu.tr>
2021-05-12 22:04:43 +0300

    mvc project
    A mvc project suitable for 3-layer architecture.

---
+++ MvcProjeKampi/DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,49 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context c = new Context();
+        DbSet<Category> _object;
+
+        public void Delete(Category p)
+        {
+            //p=parametre hepsi için
+            _object.Remove(p);
+            c.SaveChanges();
+        }
+
+        public void Insert(Category p)
+        {
+            _object.Add(p);
+            c.SaveChanges();
+        }
+
+        public List<Category> List()
+        {
+            return _object.ToList();
+        }
+
+        //şartlı listeleme için gerekiyor bu kod.
+        public List<Category> List(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category p)
+        {
+            //direk olarak değişikler kaydedilir.
+            //çünkü zaten güncellemeden önce yeni değişiklikler buraya kaydedilecek.
+            c.SaveChanges();
+        }
+    }
+}
---
+++ MvcProjeKampi/DataAccessLayer/Concrete/Repositories/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    //Böylece bütün interfaceler ve sınıflar için geçerli mimariyi kurduk.
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        Context c = new Context();
+        DbSet<T> _object;
+        //CONSTRUCTOR yaptık.obje isimli field dışarıdan gönderdiğimiz entity neyse o olacak.
+        public GenericRepository()
+        {
+            _object = c.Set<T>();
+        }
+
+
+        public void Delete(T p)
+        {
+            _object.Remove(p);
+            c.SaveChanges();
+        }
+
+        public void Insert(T p)
+        {
+            _object.Add(p);
+            c.SaveChanges();
+        }
+
+        public List<T> List()
+        {
+            return _object.ToList();
+        }
+
+        public List<T> List(Expression<Func<T, bool>> filter)
+        {
+            //filtere ne gönderirsem onu listeliyor.
+            return _object.Where(filter).ToList();
+        }
+
+        public void Update(T p)
+        {
+            c.SaveChanges();
+        }
+    }
+}
